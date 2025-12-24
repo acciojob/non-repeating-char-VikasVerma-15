0522d00bab64897bee947f10661ba109b7b696a0
VikasVerma-15 <97469119+VikasVerma-15@users.noreply.github.com>
2025-12-24 13:58:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const map = new Map();
+
+  for (let ch of str) {
+    map.set(ch, (map.get(ch) || 0) + 1);
+  }
+
+  for (let [ch, count] of map) {
+    if (count === 1) return ch;
+  }
+
+  return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
